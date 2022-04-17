f440e5524234b2c426fcb46cd185f300eeb6d716
Serking de Orayom <kingdeorayom@gmail.com>
2022-04-17 13:21:07 +0800

    added some about content

--- about.php
+++ about.php
@@ -25,18 +25,16 @@ $pagecssVersion = filemtime('./styles/custom/about.css');
 
     <main class="container">
 
-        <section class="page-control my-5 mx-2">
-
+        <section class="page-control">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="./index.php" class="content-title pb-2">serkingdeorayom</a></li>
+                    <li class="breadcrumb-item"><a href="./index.php" class="breadcrumb-title pb-2">serkingdeorayom</a></li>
                     <li class="breadcrumb-item active" aria-current="page">about me</li>
                 </ol>
             </nav>
-
         </section>
 
-        <section class="serkingdeorayom my-5">
+        <section class="introduction">
             <div class="row">
                 <div class="col-sm-12 col-md-6">
                     <div class="about-image-holder">
@@ -44,9 +42,21 @@ $pagecssVersion = filemtime('./styles/custom/about.css');
                     </div>
                 </div>
                 <div class="col-sm-12 col-md-6">
-                    <div class="content-holder">
-                        <h1 class="about-header-text display-3">hi, i'm <a href="./index.php" class="content-title pb-2">Serking</a></h1>
-                        <h5 class="about-content-text">I've spent the past 5 years working across different areas of development: desktop application, web application, UI/UX design</h5>
+                    <div class="content-holder-text">
+                        <h1 class="about-header-text display-3">hi, i'm <a href="./index.php" class="about-name pb-2">Serking</a>.</h1>
+                        <h5 class="about-content-text">i've spent the past 4 years working across different areas of development: desktop application, web application, UI/UX design.</h5>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="history">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="content-holder-text">
+                        <h1 class="about-header-text display-6">a brief history of king</h1>
+                        <h5 class="about-content-text">i was first enthralled by programming back in senior high school when i successfully made a text scroll left to right automatically using <code>&lt;marquee&gt;</code> (i know, i know, HTML is not programming)&#8212;but that paved the way for me to explore a vast new world&#8212;and boy, little did i know that i'll be staying in that world for the years to come.</h5>
+                        <h5 class="about-content-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nihil veniam quo illo nulla adipisci magni debitis sed nesciunt, non, nisi animi doloribus ex nam? Quis deleniti mollitia consectetur fugiat.</h5>
                     </div>
                 </div>
             </div>
