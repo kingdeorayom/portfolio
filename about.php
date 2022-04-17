35d3428614b8e47811bdf4aef68ce8ebe65da210
Serking de Orayom <kingdeorayom@gmail.com>
2022-04-17 19:54:46 +0800

    more about content

--- about.php
+++ about.php
@@ -37,26 +37,112 @@ $pagecssVersion = filemtime('./styles/custom/about.css');
         <section class="introduction">
             <div class="row">
                 <div class="col-sm-12 col-md-6">
-                    <div class="about-image-holder">
-                        <img src="./assets/images/kingdeorayom.jpg" alt="" class="img-fluid about-image">
+                    <div class="introduction-image-holder">
+                        <img src="./assets/images/kingdeorayom.jpg" alt="" class="img-fluid introduction-image">
                     </div>
                 </div>
                 <div class="col-sm-12 col-md-6">
-                    <div class="content-holder-text">
-                        <h1 class="about-header-text display-3">hi, i'm <a href="./index.php" class="about-name pb-2">Serking</a>.</h1>
-                        <h5 class="about-content-text">i've spent the past 4 years working across different areas of development: desktop application, web application, UI/UX design.</h5>
+                    <div class="introduction-holder-text">
+                        <h1 class="introduction-header-text display-3">hi, i'm <a href="./index.php" class="introduction-name pb-2">Serking</a>.</h1>
+                        <h5 class="introduction-content-text">i've spent the past 4 years working across different areas of development: desktop application, web application, UI/UX design</h5>
+                        <h5 class="introduction-content-text d-sm-block d-md-none d-lg-block">you may view or download my CV by clicking <a href="./assets/files/resume.pdf" target="_blank" class="download-link">here</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="row mx-auto d-none d-sm-none d-md-block d-lg-none">
+                <h5 class="introduction-content-text">you may view or download my CV by clicking <a href="./assets/files/resume.pdf" target="_blank" class="download-link">here</a></h5>
+            </div>
+        </section>
+
+        <section class="philosophy">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="philosophy-holder-text">
+                        <h1 class="philosophy-header-text display-6">this is who i am</h1>
+                        <div class="card">
+                            <div class="card-body">
+                                <blockquote class="blockquote mb-0">
+                                    <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quos voluptatem, dicta repudiandae suscipit, maiores ipsum enim laudantium et earum molestias nam reiciendis illum. Aperiam tenetur suscipit quas assumenda temporibus.&ldquo;</p>
+                                    <footer class="blockquote-footer">lorem ipsum in <cite title="Source Title">lorem ipsum</cite></footer>
+                                </blockquote>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
 
-        <section class="history">
+        <section class="hobbies">
             <div class="row">
                 <div class="col-sm-12">
-                    <div class="content-holder-text">
-                        <h1 class="about-header-text display-6">a brief history of king</h1>
-                        <h5 class="about-content-text">i was first enthralled by programming back in senior high school when i successfully made a text scroll left to right automatically using <code>&lt;marquee&gt;</code> (i know, i know, HTML is not programming)&#8212;but that paved the way for me to explore a vast new world&#8212;and boy, little did i know that i'll be staying in that world for the years to come.</h5>
-                        <h5 class="about-content-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nihil veniam quo illo nulla adipisci magni debitis sed nesciunt, non, nisi animi doloribus ex nam? Quis deleniti mollitia consectetur fugiat.</h5>
+                    <div class="hobbies-holder-text">
+                        <h1 class="hobbies-header-text display-6">what i do during my free time</h1>
+                        <h5 class="hobbies-content-text">i spend most of my free time doing either of the following:</h5>
+                        <div class="row">
+                            <div class="col-sm-12 col-md-6">
+                                <div class="hobbies-image-holder">
+                                    <img src="./assets/images/programming.png" alt="" class="img-fluid hobbies-image">
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6">
+                                <div class="hobbies-holder-text">
+                                    <h1 class="hobbies-header-text display-5">programming</h1>
+                                    <h5 class="hobbies-content-text">when not working on a project, i try to learn new programming and development concepts. currently, i'm learning <a href="https://reactjs.org/" target="_blank" class="hobbies-link"><em>React JS</em></a></h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12 col-md-6">
+                                <div class="hobbies-image-holder">
+                                    <img src="./assets/images/beatles.jpg" alt="The Beatles" class="img-fluid hobbies-image">
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6">
+                                <div class="hobbies-holder-text">
+                                    <h1 class="hobbies-header-text display-5">listen to music</h1>
+                                    <h5 class="hobbies-content-text">it's <a href="https://www.thebeatles.com/" target="_blank" class="hobbies-link"><em>The Beatles</em></a> for me. sometimes <a href="https://www.queenonline.com/" target="_blank" class="hobbies-link"><em>Queen</em></a>, sometimes <a href="https://www.ledzeppelin.com/" target="_blank" class="hobbies-link"><em>Led Zeppelin</em></a></h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12 col-md-6">
+                                <div class="hobbies-image-holder">
+                                    <img src="./assets/images/books.jpg" alt="" class="img-fluid hobbies-image">
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6">
+                                <div class="hobbies-holder-text">
+                                    <h1 class="hobbies-header-text display-5">read books</h1>
+                                    <h5 class="hobbies-content-text">i'm currently reading <em class="hobbies-link">Ang Tundo Man May Langit Din by Andres Cristobal Cruz</em></h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12 col-md-6">
+                                <div class="hobbies-image-holder">
+                                    <img src="./assets/images/chess.jpg" alt="" class="img-fluid hobbies-image">
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6">
+                                <div class="hobbies-holder-text">
+                                    <h1 class="hobbies-header-text display-5">chess</h1>
+                                    <h5 class="hobbies-content-text">i've been playing chess since i was a kid. lately, i mostly play on <a href="https://www.chess.com" target="_blank" class="hobbies-link"><em>Chess.com</em></a> if my friends aren't around</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12 col-md-6">
+                                <div class="hobbies-image-holder">
+                                    <img src="./assets/images/witcher.jpg" alt="" class="img-fluid hobbies-image">
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6">
+                                <div class="hobbies-holder-text">
+                                    <h1 class="hobbies-header-text display-5">video games</h1>
+                                    <h5 class="hobbies-content-text">i'm currently playing <a href="https://www.thewitcher.com/en/witcher3" target="_blank" class="hobbies-link"><em>The Witcher 3: Wild Hunt</em></a>, <a href="https://www.stardewvalley.net/" target="_blank" class="hobbies-link"><em>Stardew Valley</em></a> and <a href="https://www.rockstargames.com/gta-v" target="_blank" class="hobbies-link"><em>Grand Theft Auto V</em></a></h5>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
