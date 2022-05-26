fbd05e243fba68fdd2d6a846a97c8a214507f8fb
Serking de Orayom <kingdeorayom@gmail.com>
2022-05-26 16:50:35 +0800

    deleted old comments

--- about.php
+++ about.php
@@ -72,9 +72,7 @@ $aboutPhotoVersion = filemtime('./assets/images/kingdeorayom.jpg');
 
                     <div class="skills-holder-text">
 
-                        <!-- <h1 class="skills-header-text display-6">i'm not as skilled as you think i am, but</h1> -->
                         <h1 class="skills-header-text h2">i know when and how to efficiently use the following technologies</h1>
-                        <!-- <h5 class="skills-content-text">i know how and when to efficiently use the following technologies below</h5> -->
                         <div class="skills-holder-main">
                             <div class="row text-center">
                                 <div class="col-6 col-md-3">
@@ -92,7 +90,7 @@ $aboutPhotoVersion = filemtime('./assets/images/kingdeorayom.jpg');
                                 <div class="col-6 col-md-3">
                                     <div class="skills-image-holder">
                                         <img src="./assets/images/js.svg" width="75" alt="" class="img-fluid skills-image">
-                                        <h5 class="skills-content-text">JavaScript ES6</h5>
+                                        <h5 class="skills-content-text">JavaScript</h5>
                                     </div>
                                 </div>
                                 <div class="col-6 col-md-3">
@@ -129,109 +127,10 @@ $aboutPhotoVersion = filemtime('./assets/images/kingdeorayom.jpg');
                                 </div>
                             </div>
                         </div>
-                        <!-- <h5 class="skills-content-text text-center">you may view the projects i have created using the technologies above by clicking <a href="./works.php" class="download-link">here</a></h5> -->
                     </div>
                 </div>
             </div>
         </section>
-
-        <!-- <section class="philosophy">
-            <div class="row">
-                <div class="col-sm-12">
-
-                    <div class="philosophy-holder-text">
-
-                        <h1 class="philosophy-header-text display-6">a brief showcase of my personality</h1>
-
-                        <figure class="philosophy-content">
-                            <blockquote class="blockquote philosophy-header-text">
-                                <h5>&ldquo;If you can't make it good, at least make it look good.&rdquo;</h5>
-                            </blockquote>
-                            <blockquote class="blockquote philosophy-content-text">
-                                <h5>&#8211; Bill Gates on <em>Windows Vista</em>&#8482;</h5>
-                            </blockquote>
-                        </figure>
-
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="hobbies">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="hobbies-holder-text">
-                        <h1 class="hobbies-header-text display-6">what i do during my free time</h1>
-                        <h5 class="hobbies-content-text">i spend most of my free time doing either of the following:</h5>
-                        <div class="row">
-                            <div class="col-sm-12 col-md-6">
-                                <div class="hobbies-image-holder">
-                                    <img src="./assets/images/programming.png" alt="" class="img-fluid hobbies-image">
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-md-6">
-                                <div class="hobbies-holder-text">
-                                    <h1 class="hobbies-header-text display-5">programming</h1>
-                                    <h5 class="hobbies-content-text">when not working on a project, i try to learn new programming and development concepts. currently, i'm learning <a href="https://reactjs.org/" target="_blank" class="hobbies-link"><em>React JS</em></a></h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-12 col-md-6">
-                                <div class="hobbies-image-holder">
-                                    <img src="./assets/images/beatles.jpg" alt="The Beatles" class="img-fluid hobbies-image">
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-md-6">
-                                <div class="hobbies-holder-text">
-                                    <h1 class="hobbies-header-text display-5">listen to music</h1>
-                                    <h5 class="hobbies-content-text">it's <a href="https://www.thebeatles.com/" target="_blank" class="hobbies-link"><em>The Beatles</em></a> for me. sometimes <a href="https://www.queenonline.com/" target="_blank" class="hobbies-link"><em>Queen</em></a>, sometimes <a href="https://www.ledzeppelin.com/" target="_blank" class="hobbies-link"><em>Led Zeppelin</em></a></h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-12 col-md-6">
-                                <div class="hobbies-image-holder">
-                                    <img src="./assets/images/sherlock.png" alt="" class="img-fluid hobbies-image">
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-md-6">
-                                <div class="hobbies-holder-text">
-                                    <h1 class="hobbies-header-text display-5">read books</h1>
-                                    <h5 class="hobbies-content-text">i'm currently reading <a href="https://sherlock-holm.es/" target="_blank" class="hobbies-link"><em>Sherlock Holmes</em></a> novels and short stories</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-12 col-md-6">
-                                <div class="hobbies-image-holder">
-                                    <img src="./assets/images/chess.jpg" alt="" class="img-fluid hobbies-image">
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-md-6">
-                                <div class="hobbies-holder-text">
-                                    <h1 class="hobbies-header-text display-5">chess</h1>
-                                    <h5 class="hobbies-content-text">i've been playing chess since i was a kid. lately, i mostly play on <a href="https://www.chess.com" target="_blank" class="hobbies-link"><em>Chess.com</em></a> if my friends aren't around</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-12 col-md-6">
-                                <div class="hobbies-image-holder">
-                                    <img src="./assets/images/witcher.jpg" alt="" class="img-fluid hobbies-image">
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-md-6">
-                                <div class="hobbies-holder-text">
-                                    <h1 class="hobbies-header-text display-5">video games</h1>
-                                    <h5 class="hobbies-content-text">i'm currently playing <a href="https://www.thewitcher.com/en/witcher3" target="_blank" class="hobbies-link"><em>The Witcher 3: Wild Hunt</em></a>, <a href="https://www.stardewvalley.net/" target="_blank" class="hobbies-link"><em>Stardew Valley</em></a> and <a href="https://www.rockstargames.com/gta-v" target="_blank" class="hobbies-link"><em>Grand Theft Auto V</em></a></h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section> -->
 
         <section class="opportunities animate__animated animate__fadeInUp">
             <div class="row">
